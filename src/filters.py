d86400cd55441c09c0a7588f8af7fa51729f6c93
Hannu Parviainen <hannu@iac.es>
2017-09-18 16:24:17 +0100

    Added some basic transmission curves (filters) and improved TabulatedFilter.

--- src/filters.py
+++ src/filters.py
@@ -17,7 +17,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-from numpy import array, argsort, zeros_like, interp, loadtxt
+from numpy import array, argsort, zeros_like, arange, loadtxt, linspace
+from scipy.interpolate import interp1d
 
 class Filter(object):
     def __init__(self, name):
@@ -25,6 +26,15 @@ class Filter(object):
 
     def __call__(self, wl):
         raise NotImplementedError
+
+    def plot(self, ax=None, wl_min=300, wl_max=1000, wl_res=500):
+        from matplotlib.pyplot import subplots, setp
+        if ax is None:
+            fig, ax = subplots()
+        wl = linspace(wl_min, wl_max, wl_res)
+        ax.plot(wl, self(wl))
+        setp(ax, xlabel='Wavelength [nm]', ylabel='Transmission')
+        return ax
 
 
 class TabulatedFilter(Filter):
@@ -51,15 +61,22 @@ class TabulatedFilter(Filter):
         sid = argsort(self.wl)
         self.wl = self.wl[sid]
         self.tm = self.tm[sid]
-
+        self._ip = interp1d(self.wl, self.tm, kind='cubic', bounds_error=False, fill_value=0.)
 
     def __call__(self, wl):
-        return interp(wl, self.wl, self.tm, 0., 0.)
+        return self._ip(wl)
 
 
 class BoxcarFilter(Filter):
-    """A simple boxcar filter."""
+    """Boxcar filter."""
     def __init__(self, name, wl_min, wl_max):
+        """
+        Parameters
+
+        name        : str           filter name
+        wl_min      : float         minimum wavelength [nm]
+        wl_max      : float         maximum wavelength [nm]
+        """
         super(BoxcarFilter,self).__init__(name)
         self.wl_min = wl_min
         self.wl_max = wl_max
@@ -68,3 +85,16 @@ class BoxcarFilter(Filter):
         w = zeros_like(wl)
         w[(wl>self.wl_min) & (wl<self.wl_max)] = 1.
         return w
+
+sdss_g = BoxcarFilter("g'", 400, 550)
+sdss_r = BoxcarFilter("r'", 570, 690)
+sdss_i = BoxcarFilter("i'", 710, 790)
+sdss_z = BoxcarFilter("z'", 810, 900)
+kepler = TabulatedFilter('kepler',
+                         arange(350, 960, 25),
+                         array([0.000, 0.001, 0.000, 0.056, 0.465, 0.536, 0.624, 0.663,
+                                0.681, 0.715, 0.713, 0.696, 0.670, 0.649, 0.616, 0.574,
+                                0.541, 0.490, 0.468, 0.400, 0.332, 0.279, 0.020, 0.000,
+                                0.000]))
+
+__all__ = 'Filter TabulatedFilter BoxcarFilter sdss_g sdss_r sdss_i sdss_z kepler'.split()
