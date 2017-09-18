db6a1778126fa7698ecc8523ae5724acf9cc469d
Hannu Parviainen <hannu@iac.es>
2017-09-18 17:13:57 +0100

    Added a version number and version_info to the __init__.py.

--- ldtk/__init__.py
+++ ldtk/__init__.py
@@ -2,4 +2,7 @@ from .ldmodel import *
 from .ldtk import LDPSetCreator, LDPSet, load_ldpset
 from .filters import BoxcarFilter, TabulatedFilter
 
+version_info = (1, 0, 0)
+version = '.'.join(str(c) for c in version_info)
+
 __all__ = ['LDPSetCreator', 'LDPSet', 'load_ldpset', 'BoxcarFilter', 'TabulatedFilter']
