36fc458fdaa35a4ae95ae87c90f428a238cc0cb3
Hannu Parviainen <hannu@iac.es>
2019-08-21 00:10:09 +0100

    - Added the TESS response function into filters.

--- ldtk/__init__.py
+++ ldtk/__init__.py
@@ -1,8 +1,5 @@
-from .ldmodel import *
 from .ldtk import LDPSetCreator, LDPSet, load_ldpset
-from .filters import BoxcarFilter, TabulatedFilter
+from .filters import BoxcarFilter, TabulatedFilter, sdss_g, sdss_r, sdss_i, sdss_z, kepler, tess
 
 version_info = (1, 0, 0)
 version = '.'.join(str(c) for c in version_info)
-
-__all__ = ['LDPSetCreator', 'LDPSet', 'load_ldpset', 'BoxcarFilter', 'TabulatedFilter']
